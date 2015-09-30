bad4a7e054ad2b74814f50c2f50bd32d8d2c0b80
jabberwick <mr_mcmenemy@yahoo.com>
2015-09-30 04:46:53 +0000

    added production/development config options

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-var express = require("express");
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+var express = require("./config/express");
 var app = express();
 
-app.use("/", function(req, res) {
-    res.send("Hello World");
-});
-
 app.listen(process.env.PORT, process.env.IP);
-console.log("server running");
 
 module.exports = app;
+
+console.log("server running");
