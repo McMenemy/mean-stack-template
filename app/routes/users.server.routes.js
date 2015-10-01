50130bde29b448e41be07f54d4a78f1aa36e5685
jabberwick <mr_mcmenemy@yahoo.com>
2015-10-01 18:16:48 +0000

    added create, list, read, and update functions to user model

---
+++ app/routes/users.server.routes.js
@@ -0,0 +1,13 @@
+var users = require("../../app/controllers/users.server.controller");
+
+module.exports = function(app) {
+    app.route("/users")
+        .post(users.create)
+        .get(users.list);
+        
+    app.route("/users/:userID")
+        .get(users.read)
+        .put(users.update);
+        
+    app.param("userID", users.userByID);
+};
